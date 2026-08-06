f9b9854e97f139fd42baeb9624ed2d697fa30dd5
The Zulu Man <32515292+Rayrayjunior@users.noreply.github.com>
2026-08-06 22:04:04 +0200

    Name state, Amount State and button function added

---
+++ wallet/src/Components/Transactions/buystock.jsx
@@ -0,0 +1,20 @@
+import { useState, useMemo, useEffect} from "react"
+
+const buyStock = ( addtoWallet ) => {
+
+    const [name, setName] = useState("");
+    const [amount, setAmount] = useState("");
+
+    const theDetails = () => {
+
+        if(name.trim() === "") return;
+
+        const walletInfo = {
+            name : name,
+            amount : amount,
+            TansID : Date.now()
+        };
+    }
+};
+
+export default buyStock;
